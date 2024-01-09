df201a28c97379b5d7474d82e8f8d9986cf2a59b
haikaljs <haikaljavascript@gmail.com>
2024-01-09 16:16:33 +0800

    Display current user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import useCurentUser from "@/hooks/useCurrentUser";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -13,14 +14,17 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default function Home() {
+  const { data: user } = useCurentUser();
+  console.log(user);
   return (
     <>
       <h1 className="text-red-500 text-6xl">Netflix clone</h1>
+      <p className="text-white">Logged in: {user?.email}</p>
       <button onClick={() => signOut()} className="w-full h-10 bg-white">
         Logout
       </button>
