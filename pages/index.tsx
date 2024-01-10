07130465669267f8927947ddd0a976af2cbf8128
haikaljs <haikaljavascript@gmail.com>
2024-01-10 20:16:11 +0800

    Navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPageContext } from "next";
 import { getSession} from "next-auth/react";
 
+import Navbar from "@/components/Navbar";
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,7 +24,7 @@ export default function Home() {
 
   return (
     <>
-    
+    <Navbar />
     </>
   );
 }
